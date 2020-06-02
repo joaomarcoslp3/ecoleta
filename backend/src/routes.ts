1879e03aa3e2f936bad05c1a689eb58634b48ce6
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-06-02 20:05:32 -0300

    database controllers created

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import PointsControllers from './controllers/PointsController';
+import ItemsControllers from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsControllers();
+const itemsController = new ItemsControllers();
 
-routes.get('/', (req, res)=> {
-  return res.json({ message: 'Access' })
-});
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
 
 export default routes;
